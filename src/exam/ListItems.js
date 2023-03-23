e75ac1d0088298a0c6ef61686530f4fed7618ac4
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2023-03-23 18:00:47 +0100

    codevixen-exam-one

---
+++ src/exam/ListItems.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const ListItems = (props) => {
+  const [selectedItems, setSelectedItems] = React.useState([]);
+  const handleChange = (e, id) => {
+    if (e.target.checked) {
+      setSelectedItems([...selectedItems, id]);
+    } else {
+      const newSelectedItems = selectedItems.filter((item) => item !== id);
+      setSelectedItems(newSelectedItems);
+    }
+  };
+  const handleClick = () => {
+    alert(`Selected item IDs: ${selectedItems.join(', ') || 'none'}`);
+  };
+
+  return (
+    <div>
+      {props.items.length === 0 ? (
+        <p>No items to display</p>
+      ) : (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Selected</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.items.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={selectedItems.includes(item.id)}
+                      onChange={(e) => handleChange(e, item.id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button onClick={handleClick}>Show Selected Item IDs</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ListItems;
